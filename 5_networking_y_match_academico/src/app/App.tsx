68681b69be6074ce602fc0cc4a9dfb38ad3fada9
Maria Lopez <102682411+marialopezret@users.noreply.github.com>
2026-06-03 00:26:03 -0400

    Update App.tsx

--- 5_networking_y_match_academico/src/app/App.tsx
+++ 5_networking_y_match_academico/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router-dom'; // 👈 Cambiado desde 'react-router'
 import { router } from './routes';
 
 export default function App() {
